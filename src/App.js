c4b7f93f5c7c681b7d87a2ae0246a0fdef426e70
Maxime Fabas <maxime.fabas@pm.me>
2020-04-28 16:09:00 +0200

    job done

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ export default class App extends Component {
       error_sheet: null,
       data_sheet: [],
       keystrokes_history: [],
+      random_value_for_gif: Math.random(),
       konami_mode: false
     }
     this.fetchSheet = this.fetchSheet.bind(this)
@@ -108,8 +109,16 @@ export default class App extends Component {
       const reach = await window.fetch(this.props.spreadsheet)
       if (!reach.ok) throw reach
       const data = await reach.text()
-      const parsedData = parseTsv(data, [7])[0]
-      this.setState({ loading_sheet: false, error_sheet: null, data_sheet: parsedData })
+      const usableData = { gifs: [] }
+      const parsedData = data
+        .split(/\n/)
+        .map(line => line.trim().split(/\t/)).forEach(pair => {
+          const trimmedKey = pair[0] ? pair[0].trim() : null
+          const trimmedVal = pair[1] ? pair[1].trim() : null
+          if (trimmedKey !== 'gif') usableData[trimmedKey] = trimmedVal
+          else usableData.gifs.push(trimmedVal)
+        })
+      this.setState({ loading_sheet: false, error_sheet: null, data_sheet: [usableData] })
       return data
     } catch (error) {
       if (error.status) {
@@ -166,10 +175,31 @@ export default class App extends Component {
     if (state.loading_sheet) return <div className={classes.join(' ')}><div className='lblb-default-apps-loader'><Loader /></div></div>
     if (state.error_sheet) return <div className={classes.join(' ')}><div className='lblb-default-apps-error'><LoadingError /></div></div>
 
-    const { end_date: endDate, end_time: endTime, no, yes, today, passed } = state.data_sheet[0]
+    const {
+      end_date: endDate,
+      end_time: endTime,
+      question,
+      no,
+      yes,
+      today,
+      passed,
+      links_title: linksTitle,
+      link_1_url: link1url,
+      link_1_title: link1title,
+      link_2_url: link2url,
+      link_2_title: link2title,
+      link_3_url: link3url,
+      link_3_title: link3title,
+      link_4_url: link4url,
+      link_4_title: link4title,
+      link_5_url: link5url,
+      link_5_title: link5title,
+      link_6_url: link6url,
+      link_6_title: link6title,
+      gifs
+    } = state.data_sheet[0]
     const endMomentStr = `${endDate} ${endTime}`
     const endMoment = moment(endMomentStr, 'YYYY/MM/DD HH:mm')
-    // const nowMoment = moment('2020/05/11 13:30', 'YYYY/MM/DD HH:mm')
     const nowMoment = moment()
     const daysDiff = endMoment.diff(nowMoment, 'days', true)
     const isPassed = endMoment.diff(nowMoment) <= 0
@@ -177,14 +207,19 @@ export default class App extends Component {
     const nowMomentToMidnight = moment(nowMoment).startOf('day')
     const diffToMidnight = endMomentToMidnight.diff(nowMomentToMidnight, 'days')    
 
-    const question = state.data_sheet[0].question
+    const randomGif = gifs[Math.floor(state.random_value_for_gif * gifs.length)]
+    const readableEndMoment = endMoment.from(nowMoment)
+      .replace(/il\sy\sa\s/igm, '')
+      .replace(/dans\s/igm, '')
+      .replace(/\s[a-zA-Z]*$/, match => match.replace(/\s/, ' '))
     const answer = diffToMidnight > 1
-      ? no.replace(/<<MOMENT>>/igm, endMoment.from(nowMoment).replace(/il\sy\sa\s/igm, '').replace(/dans\s/igm, ''))
+      ? no.replace(/<<MOMENT>>/igm, readableEndMoment)
       : diffToMidnight === 1
-      ? yes.replace(/<<MOMENT>>/igm, endMoment.from(nowMoment).replace(/il\sy\sa\s/igm, '').replace(/dans\s/igm, ''))
+      ? yes.replace(/<<MOMENT>>/igm, readableEndMoment)
       : isPassed
-      ? passed.replace(/<<MOMENT>>/igm, endMoment.from(nowMoment).replace(/il\sy\sa\s/igm, '').replace(/dans\s/igm, ''))
-      : today.replace(/<<MOMENT>>/igm, endMoment.from(nowMoment).replace(/il\sy\sa\s/igm, '').replace(/dans\s/igm, ''))
+      ? passed.replace(/<<MOMENT>>/igm, readableEndMoment)
+      : today.replace(/<<MOMENT>>/igm, readableEndMoment)
+    const tweet = state.data_sheet[0].tweet.replace(/<<URL>>/igm, window.location.href)
 
     /* Display component */
     return <div className={classes.join(' ')}>
@@ -198,14 +233,20 @@ export default class App extends Component {
           <JSXInterpreter content={answer} />
         </InterTitle>
       </div>
-      <div className={`${c}__links`}>
-        <AnnotationTitle big>Pour passer le temps</AnnotationTitle>
-        <Paragraph><a href='/'>Un lien</a></Paragraph>
-        <Paragraph><a href='/'>Un lien</a></Paragraph>
-        <Paragraph><a href='/'>Un lien</a></Paragraph>
+      <div className={`${c}__gif`}>
+        <img src={randomGif} />
       </div>
+      {link1url && <div className={`${c}__links`}>
+        {link1url && <AnnotationTitle big>{linksTitle}</AnnotationTitle>}
+        {link1url && <Paragraph><a href={link1url}>{link1title}</a></Paragraph>}
+        {link2url && <Paragraph><a href={link2url}>{link2title}</a></Paragraph>}
+        {link3url && <Paragraph><a href={link3url}>{link3title}</a></Paragraph>}
+        {link4url && <Paragraph><a href={link4url}>{link4title}</a></Paragraph>}
+        {link5url && <Paragraph><a href={link5url}>{link5title}</a></Paragraph>}
+        {link6url && <Paragraph><a href={link6url}>{link6title}</a></Paragraph>}
+      </div>}
       <div className='lblb-default-apps-footer'>
-        <ShareArticle short iconsOnly tweet={props.meta.tweet} url={props.meta.url} />
+        <ShareArticle short iconsOnly tweet={tweet} url={' '} />
         <LibeLaboLogo target='blank' />
       </div>
     </div>
--- src/config.js
+++ src/config.js
@@ -4,17 +4,17 @@ const currentHostname = typeof window !== 'undefined' ? window.location.hostname
 const config = {
   meta: {
     author: 'Libé Labo',
-    title: 'Est-ce que c\'est demain la fin du confinement ?',
-    url: '',
-    description: '',
-    image: '',
-    xiti_id: 'est-ce-que-c-est-demain',
-    tweet: 'Some tweet text',
+    title: 'Est-ce que c\'est le déconfinement ?',
+    url: 'https://www.liberation.fr/apps/2020/04/est-ce-que-c-est-le-deconfinement',
+    description: 'Combien de jours nous séparent du déconfinement',
+    image: 'https://www.liberation.fr/apps/2020/04/est-ce-que-c-est-le-deconfinement/social.jpg',
+    xiti_id: 'est-ce-que-c-est-le-deconfinement',
+    tweet: 'Est-ce qu\'on est bientôt déconfinés ?',
   },
   tracking: {
     active: false,
-    format: 'est-ce-que-c-est-demain',
-    article: 'est-ce-que-c-est-demain'
+    format: 'est-ce-que-c-est-le-deconfinement',
+    article: 'est-ce-que-c-est-le-deconfinement'
   },
   show_header: true,
   statics_url: process.env.NODE_ENV === 'production'
@@ -23,7 +23,7 @@ const config = {
   api_url: process.env.NODE_ENV === 'production'
     ? 'https://libe-labo-2.site/api'
     : `${currentProtocol}//${currentHostname}:3004/api`,
-  stylesheet: 'est-ce-que-c-est-demain.css',
+  stylesheet: 'est-ce-que-c-est-le-deconfinement.css',
   spreadsheet: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRkd5cstwwUfeFgzBcwOuYWwPPpdiB3wkXaUHkWew8gahb9_9M_MhuWjSg928IoPjWai9aVfWG3F8H9/pub?gid=0&single=true&output=tsv'
 }
 
